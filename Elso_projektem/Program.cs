0321dfcc7b9c2525e82010e191d103b465df7db0
varzoltan <64203391+varzoltan@users.noreply.github.com>
2022-09-06 12:43:51 +0200

    Add project files.

---
+++ Elso_projektem/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;//gyűjtemény, kollekció, .NET keretrendszernek
+//using System.IO;//Fájlok beolvasására és kiírásához importált osztályokat tartalmazza.
+
+//Egysoros megjegyzés
+
+/*
+ * Többsoros megjegyzés
+ * aékjsckéac
+ * éakéi
+ */
+
+namespace Elso_projektem//névtér
+{
+    internal class Program//Névtéren belüli automatikusan létrejövő osztály
+    {
+        static void Main(string[] args)//Főprogram
+        {
+            //változó deklarálása, inicializálása
+            //változó típusa szóköz változó neve (nem lehet foglalt szó vagy kulcsszó) = érték vagy kifejezés
+            int szam1 = 5;
+            
+            Console.WriteLine("Első programom!!!");
+
+            Console.ReadKey();
+        }
+    }
+}
